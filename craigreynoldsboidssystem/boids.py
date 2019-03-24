c00dc40e3fe1f944db49eb86e4243cd352a00bbd
Hong Yi Xiong <xionghongyi@gmail.com>
2019-03-24 15:20:41 -0400

    - Added a class for boids simulation.

--- craigreynoldsboidssystem/boids.py
+++ craigreynoldsboidssystem/boids.py
@@ -2,6 +2,69 @@ import random
 import tkinter
 
 
+class BoidsSimulation:
+    def __init__(self, length, width, num_boids):
+        self.num_boids = num_boids
+        self.length = length
+        self.width = width
+        # todo: the following parameters can be changed.
+        self.neighbour_radius = 10
+        self.max_velocity = 10
+
+    def boids(self):
+        self.initialise_boids(self.num_boids)
+        self.move_all_boids_to_new_positions()
+
+    def initialise_boids(self, num_boids):
+        """
+        Initialise boids at random positions and velocity.
+        :param num_boids: the number of boids to be created.
+        """
+        pass
+
+    def move_all_boids_to_new_positions(self):
+        """
+        Moves all boids to new positions.
+        :return:
+        """
+        pass
+
+    def rule1(self):
+        """
+        This rule simulates flock cohesion.
+        :return:
+        """
+        pass
+
+    def rule2(self):
+        """
+        This rule simulates separation.
+        :return:
+        """
+        pass
+
+    def rule3(self):
+        """
+        This rule simulates alignment.
+        :return:
+        """
+        pass
+
+    def rule4(self):
+        """
+        This rule simulates wind.
+        :return:
+        """
+        pass
+
+    def rule5(self):
+        """
+        This rule simulates perching.
+        :return:
+        """
+        pass
+
+
 class Boid:
     def __init__(self):
         pos_x = random.random()
@@ -13,70 +76,12 @@ class Boid:
         self.velocity = (0, 0)
 
 
-def boids(n):
-    initialise_boids(n)
-    move_all_boids_to_new_positions()
-
-
-def initialise_boids(n):
-    """
-    Initialise boids at random positions and velocity.
-    :param n: the number of boids to be created.
-    """
-    pass
-
-
-def move_all_boids_to_new_positions():
-    """
-    Moves all boids to new positions.
-    :return:
-    """
-    pass
-
-
-def rule1():
-    """
-    This rule simulates flock cohesion.
-    :return:
-    """
-    pass
-
-
-def rule2():
-    """
-    This rule simulates separation.
-    :return:
-    """
-    pass
-
-
-def rule3():
-    """
-    This rule simulates alignment.
-    :return:
-    """
-    pass
-
-
-def rule4():
-    """
-    This rule simulates wind.
-    :return:
-    """
-    pass
-
-
-def rule5():
-    """
-    This rule simulates perching.
-    :return:
-    """
-    pass
-
-
 def main():
-    num_of_boids = 20
-    boids(num_of_boids)
+    num_boids = 20
+    length = 100
+    width = 100
+    boids_simulation = BoidsSimulation(length, width, num_boids)
+    boids_simulation.boids()
 
 
 main()
